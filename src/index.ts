e86aa75372e166fac073a6964b8c0cba50c1c21c
Ax-SylvesterHommuk <ax-sylvester.hommuk@khk.ee>
2023-01-12 09:17:27 +0200

    Ülesanne TS-3 - võimsuste arvutamine objektiga

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,49 @@
 class Resistor {
-    r: number = 0;
-    
-    constructor(r: number) {
-        this.r = r;
-    }
+  r: number = 0;
+  maxPower: number = 0;
+  maxVoltage: number = 0;
 
-    getCurrent(u: number): number {
-        return u / this.r;
-    }
-
-    getPower(u: number): number {
-        return u * this.getCurrent(u);
-    }
-
-    getAmps(w: number, v:number): number {
-        return  w / v
-    }
-
-    getOhms(v: number, w:number): number{
-        return v / this.getAmps(w, v);
-    }
-
-    getWater(w: number, ml:number): number{
-        return Math.floor(20 + (60 /(4.19 * (ml / w))))
-    }
+  constructor(r: number, maxPower: number, maxVoltage: number) {
+    this.r = r;
+    this.maxPower = maxPower;
+    this.maxVoltage = maxVoltage;
+  }
+  getCurrent(u: number): number {
+    return u / this.r;
+  }
+  getPower(u: number): number {
+    return u * this.getCurrent(u);
+  }
+  getMaxPower(): number {
+    return this.maxPower;
+  }
+  isVoltageAllowed(u: number): boolean {
+    return u <= this.maxVoltage;
+  }
 }
 
-// Takistile mõjub pinge 5 volti ning seda läbib vool 2 amprit. Mitu vatti soojust eraldub takistist?
-let r1 = new Resistor(2.5);
-console.log(r1.getPower(5));
+// Lisa takistile väli lubatud maksimumvõimsuse kohta. Selle sisestamine on vajalik konstruktoris.
+// r, maxPower, maxVoltage
+let r1 = new Resistor(220, 100, 20);
 
-// Takistile mõjub pinge 4 volti ning sealt eraldub võimsus 6 vatti. Mitu amprit voolu läbib takistit?
-console.log(r1.getAmps(6,4));
+// Loo käsk kontrollimaks, kas parameetrina antud pinge on vastava takisti puhul lubatud - st. kas pingestamisel eralduv võimsus jääb lubatud maksimuvõimsuse piiresse
+console.log(r1.isVoltageAllowed(19)); // true
+console.log(r1.isVoltageAllowed(21)); // false
 
-// Mitu oomi on eelneva takisti takistus?
-console.log(r1.getOhms(4,6))
+// Koosta takistitest massiiv. Koosta funktsioon, mis loob uue massiivi takistitest, mille lubatud võimsus jääb etteantud pinge korral lubatud piiresse.
+// r, maxPower, maxVoltage
+const resistors: Resistor[] = [
+    new Resistor(220, 100, 10),
+    new Resistor(330, 200, 15),
+    new Resistor(100, 50, 5),
+    new Resistor(100, 70, 8)
+];
 
-// Veekeedukannu võimsuseks on 1 kilovatt, seal sees on vett 1 liiter. Mitme kraadi peale tõuseb vee temperatuur 20 kraadi Celsiuse pealt ühe minutiga, kui kogu sisselülitatud kannu võimsus läheb vee soojendamiseks?
-console.log(r1.getWater(1000, 1000))
+function getAllowedResistor(voltage: number, resistors: Resistor[]): Resistor[] {
+    return resistors.filter(resistor => resistor.isVoltageAllowed(voltage));
+}
 
-// Mitu amprit voolu läbib eelnimetatud veekeedukannu, kui võrgupinge on 220 volti?
-console.log(r1.getAmps(1000, 220))
 
-// Mitu oomi on selle veekeedukannu takistus?
-console.log(r1.getOhms(220, 1000))
+// voltageAmount, resistors dict
+let allowedResistor = getAllowedResistor(8, resistors); // vastus: 3, kuna üks on alla 8 volti
+console.log(allowedResistor)
