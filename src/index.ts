060c6a6f32d959138f85c7cb89df923b9e5dc0d0
Ax-SylvesterHommuk <ax-sylvester.hommuk@khk.ee>
2023-01-13 09:07:04 +0200

    Ülesanne TS-4 - jadaühenduse arvutamine

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,6 @@ class Resistor {
     getResistance(): number {
         return this.r;
     }
-    getPotential(current: number): number {
-        return current * this.r;
-    }
 }
 
 class SeriesCircuit {
@@ -30,82 +27,70 @@ class SeriesCircuit {
     getCurrent(u: number) {
         return u / this.getTotalResistance();
     }
-    getTotalPower(u: number) {
-        return this.getCurrent(u) * u;
-    }
-    getBiggestResistorPower(u: number) {
-        if (this.resistors.length == 0) { return 0; }
-        let current: number = this.getCurrent(u);
-        let biggestPower: number = this.resistors[0].getPower(this.resistors[0].getPotential(current));
-        for (let i = 1; i < this.resistors.length; i++) {
-            let power: number = this.resistors[i].getPower(this.resistors[i].getPotential(current));
-            if (power > biggestPower) { biggestPower = power; }
-        }
-        return biggestPower;
-    }
-    getBiggestResistorResistance(): number {
-        if (this.resistors.length == 0) {
-            return 0; 
-        }
-
-        let biggestResistance = this.resistors[0].getResistance();
-
-        for (let i = 1; i < this.resistors.length; i++) {
-            let resistance = this.resistors[i].getResistance();
-            if (resistance > biggestResistance) { 
-                biggestResistance = resistance;
-            }
-        }
-        return biggestResistance;
-    }
-    getBiggestVoltageDrop(u: number): number {
-        if (this.resistors.length == 0) {
-             return 0; 
-        }
-
-        let current = this.getCurrent(u);
-        let biggestVoltageDrop = this.resistors[0].getResistance() * current;
-
-        for (let i = 1; i < this.resistors.length; i++) {
-            let voltageDrop = this.resistors[i].getResistance() * current;
-            if (voltageDrop > biggestVoltageDrop) {
-                 biggestVoltageDrop = voltageDrop; 
-            }
-        }
-        return biggestVoltageDrop;
-    }
 }
 
 // let sc1: SeriesCircuit = new SeriesCircuit();
 // sc1.push(new Resistor(220));
 // sc1.push(new Resistor(220));
-// sc1.push(new Resistor(330));
+// sc1.push(new Resistor(110));
 // console.log(sc1.getTotalResistance());
 // console.log(sc1.getCurrent(12));
 
-// Koosta jadaühenduse klassist kaks eksemplari, katseta tulemusi mitmesuguse pinge korral. Võrdle käsitsi arvutatud tulemusi programmi pakutuga.
 let sc1: SeriesCircuit = new SeriesCircuit();
+sc1.push(new Resistor(220));
+sc1.push(new Resistor(220));
+sc1.push(new Resistor(220));
+
+let current = sc1.getCurrent(12);
+console.log("Current: " + current.toFixed(4));
+
+let v1 = current * sc1.resistors[0].getResistance();
+console.log("Voltage across first resistor: " + v1 + "V");
+
+let v2 = current * sc1.resistors[1].getResistance();
+console.log("Voltage across second resistor: " + v2 + "V");
+
+let v3 = current * sc1.resistors[2].getResistance();
+console.log("Voltage across third resistor: " + v3 + "V");
+
+let p1 = sc1.resistors[0].getPower(v1);
+console.log("Power dissipated by first resistor: " + p1.toFixed(4) + "W");
+
+let p2 = sc1.resistors[1].getPower(v2);
+console.log("Power dissipated by second resistor: " + p2.toFixed(4) + "W");
+
+let p3 = sc1.resistors[2].getPower(v3);
+console.log("Power dissipated by third resistor: " + p3.toFixed(4) + "W");
+
+console.log("------------------------------------------------------------------");
+
 let sc2: SeriesCircuit = new SeriesCircuit();
+sc2.push(new Resistor(110));
+sc2.push(new Resistor(220));
+sc2.push(new Resistor(220));
 
-sc1.push(new Resistor(220));
-sc1.push(new Resistor(220));
-sc1.push(new Resistor(330));
-console.log("sc1 Total Resistance: " + sc1.getTotalResistance());
-console.log("sc1 Current at 10V: " + sc1.getCurrent(10));
-console.log("sc1 Current at 20V: " + sc1.getCurrent(20));
-console.log("sc1 Total Power at 10V: " + sc1.getTotalPower(10)); // kogu eralduva võimsus
-console.log("sc1 Biggest Resistor Resistance: " + sc1.getBiggestResistorResistance());
-console.log("sc1 Biggest Voltage Drop at 5V: " + sc1.getBiggestVoltageDrop(5) + "V"); // langev pinge 5 volti all
-console.log("sc1 Biggest Resistor Power: " + (sc1.getBiggestResistorPower(12)));
+console.log("Total Series Resistance: " + sc2.getTotalResistance() + " ohms");
 
-console.log("-------------------------------------------------")
+let current2 = sc2.getCurrent(12);
+console.log("Current through series: " + current2.toFixed(5) + "A");
 
-sc2.push(new Resistor(100));
-sc2.push(new Resistor(100));
-console.log("sc2 Total Resistance: " + sc2.getTotalResistance());
-console.log("sc2 Current at 10V: " + sc2.getCurrent(10));
-console.log("sc2 Current at 20V: " + sc2.getCurrent(20));
-console.log("sc2 Total Power at 10V: " + sc2.getTotalPower(10)); // kogu eralduva võimsus
-console.log("sc2 Biggest Resistor Resistance: " + sc2.getBiggestResistorResistance());
-console.log("sc2 Biggest Voltage Drop at 5V: " + sc2.getBiggestVoltageDrop(5) + "V"); // langev pinge 5 volti all
-console.log("sc2 Biggest Resistor Power: " + (sc2.getBiggestResistorPower(12)));
+let v1_2 = current * sc2.resistors[0].getResistance();
+console.log("Voltage across first resistor: " + v1_2 + "V");
+
+let v2_2 = current * sc2.resistors[1].getResistance();
+console.log("Voltage across second resistor: " + v2_2 + "V");
+
+let v3_2 = current * sc2.resistors[2].getResistance();
+console.log("Voltage across third resistor: " + v3_2 + "V");
+
+let p1_2 = sc2.resistors[0].getPower(v1_2);
+console.log("Power dissipated by first resistor: " + p1_2.toFixed(4) + "W");
+
+let p2_2 = sc2.resistors[1].getPower(v2_2);
+console.log("Power dissipated by first resistor: " + p2_2.toFixed(4) + "W");
+
+let p3_2 = sc2.resistors[2].getPower(v3_2);
+console.log("Power dissipated by first resistor: " + p3_2.toFixed(4) + "W");
+
+
+
